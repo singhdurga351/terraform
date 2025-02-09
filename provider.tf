3fc29d3667f1a69203201d31b0773b2d0cbc09f4
Durga Prasad Singh <141853809+singhdurga351@users.noreply.github.com>
2025-02-09 22:03:27 +0530

    Update provider.tf
    Updated

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = var.region # Update with your desired region
-  access_key = var.access
-  secret_key = var.secret
+  access_key = var.access #better to launch an ec2 amd attach IAM to ec2 with adminfullAccess
+  secret_key = var.secret #or install aws cli in same terraform machine and store creds in .aws folder
 }
