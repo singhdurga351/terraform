74acffea02acfe761d22f336b5f2d7ca5ac18859
Durga Prasad Singh <141853809+singhdurga351@users.noreply.github.com>
2025-02-09 22:05:19 +0530

    Update key.tf
    Ssh keys instructions

--- key.tf
+++ key.tf
@@ -1,4 +1,4 @@
 resource "aws_key_pair" "deployer" {
   key_name   = var.key
-  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA0NiHILmr2GBvRbH9HTpSxUbISOjAIT3bDoe6kXNUtw AzureAD+DurgaPrasadSingh@RCCHN0322"
+  public_key = "#create ssh-keygen in local machin and paste the pub key here"
 }
